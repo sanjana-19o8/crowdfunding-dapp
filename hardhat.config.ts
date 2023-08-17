10d544ed5d745c17c98ad78f44f858a7c0c087a9
sanjana-19o8 <sanjanavajrapu19@gmail.com>
2023-08-17 14:58:15 +0530

    init

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,13 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+require("@nomiclabs/hardhat-waffle");
+
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
+  networks: {
+    hardhat: {},
+  },
 };
 
 export default config;
