f9d73ce31f9e35ad563ba4de52f79d0a3ba61506
sanjana-19o8 <sanjanavajrapu19@gmail.com>
2023-08-22 22:09:27 +0530

    ayayyaya

--- frontend/scripts/deploy.ts
+++
@@ -1,5 +0,0 @@
-import { ethers } from 'hardhat'
-
-async function main() {
-    console.log('deploying contract...')
-}
--- test/Fundfactory.js
+++ test/Fundfactory.js
@@ -3,7 +3,7 @@ const hre = require("hardhat");
 
 describe("FundFactory", function () {
   it("should manage and display all fund raiser campaigns", async function () {
-    const fundFactory = await hre.ethers.deployContract("ArcadeCats");
+    const fundFactory = await hre.ethers.deployContract("Fundfactory");
     await fundFactory.waitForDeployment();
 
     console.log('Contract deployed to ', fundFactory.target);
